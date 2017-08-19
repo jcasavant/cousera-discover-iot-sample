09a92a28f08c57d185eacc4e410f0f71abb43dd6
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2017-08-19 18:19:23 +0200

    changed clientID

--- public/iot-phone/js/device.js
+++ public/iot-phone/js/device.js
@@ -77,14 +77,12 @@
 			contentType: "application/json; charset=utf-8",
 			dataType: "json",
 			success: function(response) {
-                console.log("1");
 				console.log(response);
-                console.log("2");
 				window.iot_host = response.org + ".messaging.internetofthings.ibmcloud.com";
 				window.iot_port = 8883;
 				window.deviceId = prompt("Enter a unique ID for your device containing only letters and numbers:");
 				window.password = prompt("Enter an 8-character password");
-				window.iot_clientid = "a:"+response.org+":iot-phone:"+window.deviceId;
+				window.iot_clientid = "d:"+response.org+":iot-phone:"+window.deviceId;
 				window.client = new Paho.MQTT.Client(window.iot_host, window.iot_port, window.iot_clientid);
 				registerDevice();
 			},
